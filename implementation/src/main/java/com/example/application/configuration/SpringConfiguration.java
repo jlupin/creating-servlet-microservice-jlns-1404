1444f38b2df9c6c7300a9639341f153d21758e4b
Piotr Heilman <piotr.heilman@jlupin.com>
2017-10-10 14:53:24 +0200

    Initial commit.

---
+++ implementation/src/main/java/com/example/application/configuration/SpringConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.application.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@ComponentScan("com.example.application")
+public class SpringConfiguration {
+}
